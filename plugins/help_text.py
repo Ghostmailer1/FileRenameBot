08078b0e7c8f98dc52974c1d60013eba7187fbea
Ghostmailer1 <74950510+Ghostmailer1@users.noreply.github.com>
2021-02-03 17:31:26 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -70,12 +70,12 @@ async def start(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Channel', url='https://t.me/InFoTelGroup'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/Physic_hybrid')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('My Dev👮‍♂️', url='https://t.me/Sunisk'),
+                    InlineKeyboardButton('Maintained By👮‍♂️', url='https://t.me/Physic_hybrid')
                 ]
             ]
         ),
